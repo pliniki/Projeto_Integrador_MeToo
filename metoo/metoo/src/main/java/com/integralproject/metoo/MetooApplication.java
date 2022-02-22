34940b38315d8053664aa57d874c69f93b78ec54
WagnerCarmo <amorim.carmo@hotmail.com>
2022-02-22 10:23:17 -0300

    Tarefa: 3

---
+++ metoo/metoo/src/main/java/com/integralproject/metoo/MetooApplication.java
@@ -0,0 +1,13 @@
+package com.integralproject.metoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetooApplication.class, args);
+	}
+
+}
